fd1770e9113cd72a163362a507c5c372c412cf1e
Wouter Leijs <w.leijs1@student.avans.nl>
2021-06-29 14:34:51 +0200

    Eindopdracht afgemaakt en gecomment

--- eindopdracht/eindopdracht/main.c
+++ eindopdracht/eindopdracht/main.c
@@ -17,8 +17,8 @@
 #define SPEAKER_DDR DDRC
 #define SPEAKER_PIN 7
 
-	int counter_in_ms = 0;
-	int temp = 1;
+int counter_in_ms = 0; //Counter that counts the amount of time it took for the signal to return.
+char signalRecieved = 1; //Used as a Boolean to show that the ultrasonic sensor received a signal.
 	
 void PLAYNOTE(float duration, float frequency);
 
@@ -29,48 +29,45 @@ void wait( int ms )
 	}
 }
 
+//This interrupt gets triggered when the ultrasonic sensor(echo) receives a signal.
 ISR( INT1_vect ) {
-	PLAYNOTE(100, 100);
-	temp = 0;
-}
-ISR( INT0_vect ) {
-	PLAYNOTE(100, 100);
-	temp = 0;
+	signalRecieved = 1;
 }
 
-
-int main(void)
-{
-	DDRD = 0b01100100; // used for sensor pin 0 is for trigger pin 1 is for echo
-
-	// Init Interrupt hardware
-	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
-	EIMSK |= 0x02;			// Enable INT1 & INT0
-	
-	sei();
-	
-	PLAYNOTE(200, 250);
-	while(1){
-		check_ultrasonic();
-	    PLAYNOTE(500,counter_in_ms);
-    }
-	return 1;
-}
-
-void check_ultrasonic(){
-	temp = 1;
+//This method sends a signal from the ultrasonic sensor
+void checkUltrasonic(){
+	signalRecieved = 0;
 	counter_in_ms = 0;
-	PORTD = 0b00000100;
-	_delay_us(10);
-	PORTD = 0b00000100;
+	PORTD = 0b00000100;			// Starts sending a signal
+	_delay_us(10);				// Waits for 10 ms
+	PORTD = 0b00000000;			// Stops sending a signal
 	while(1){
-		counter_in_ms = counter_in_ms + 100;
-		if(temp == 0){
+		counter_in_ms = counter_in_ms + 1;
+		if(signalRecieved == 1){
 			break;
 		}
 	}
 }
 
+int main(void)
+{
+	DDRD = 0b01100100; // used for sensor pin 2 is for trigger pin 1 is for echo
+	
+	// Init Interrupt hardware
+	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge 
+	EIMSK |= 0x02;			// Enable INT1 & INT0
+	
+	sei();					
+	
+	PLAYNOTE(1000, 250);		//Starting tone to show the program has started
+	while(1){
+		checkUltrasonic();
+	    PLAYNOTE(500,counter_in_ms/4);	
+    }
+	return 1;
+}
+
+//This method controls the sound emitted by the piëzo-element, using a specified frequency and duration.
 void PLAYNOTE(float duration, float frequency){
 	long int i, cycles;
 	float half_period;
